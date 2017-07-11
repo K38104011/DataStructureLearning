c863c32331789dd8b5c2b1ab7036d882ffc99746
GiangPDH <phamdanghoanggiang@gmail.com>
2017-07-11 19:41:39 +0700

    bubble sort

---
+++ Bubble.Sort.First.Try/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Bubble.Sort.First.Try
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int Min = 0;
+            int Max = 200;
+
+            Random randNum = new Random();
+            int[] numbers = Enumerable
+                .Repeat(0, randNum.Next(10, 20))
+                .Select(i => randNum.Next(Min, Max))
+                .ToArray();
+
+            foreach (var number in numbers)
+            {
+                Console.Write(number + ",");
+            }
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length - 1 - i; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j + 1];
+                        numbers[j + 1] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+            
+            Console.WriteLine();
+
+            foreach (var number in numbers)
+            {
+                Console.Write(number + ",");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
