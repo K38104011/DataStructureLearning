9e7cc956a1c9964f19023a9c02c5c1cf3881f178
GiangPDH <phamdanghoanggiang@gmail.com>
2017-07-11 20:36:02 +0700

    add selection sort

---
+++ Selection.Sort.First.Try/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace Selection.Sort.First.Try
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Min = 0;
+            int Max = 200;
+
+            Random randNum = new Random();
+            int[] numbers = Enumerable
+                .Repeat(0, randNum.Next(10, 20))
+                .Select(i => randNum.Next(Min, Max))
+                .ToArray();
+
+            foreach (var number in numbers)
+            {
+                Console.Write(number + ",");
+            }
+
+            var subNumbers = new int[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var smallest = 0;
+                for (int j = 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[smallest])
+                    {
+                        smallest = j;
+                    }
+                }
+                subNumbers[i] = numbers[smallest];
+                numbers[smallest] = int.MaxValue;
+            }
+
+            Console.WriteLine();
+
+            foreach (var number in subNumbers)
+            {
+                Console.Write(number + ",");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Selection.Sort.Second.Try/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace Selection.Sort.Second.Try
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Min = 0;
+            int Max = 200;
+
+            Random randNum = new Random();
+            int[] numbers = Enumerable
+                .Repeat(0, randNum.Next(10, 20))
+                .Select(i => randNum.Next(Min, Max))
+                .ToArray();
+
+            foreach (var number in numbers)
+            {
+                Console.Write(number + ",");
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var smallest = 0;
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[smallest])
+                    {
+                        smallest = j;
+                    }
+                }
+                Swap(ref numbers[smallest], ref numbers[i]);
+            }
+
+            Console.WriteLine();
+
+            foreach (var number in numbers)
+            {
+                Console.Write(number + ",");
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            a = a + b;
+            b = a - b;
+            a = a - b;
+        }
+    }
+}
