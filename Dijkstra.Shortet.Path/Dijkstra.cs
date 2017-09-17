067a28b24c1b2b7acbbb8e084e23cbc695c9659e
Giang <phamdanghoanggiang@gmail.com>
2017-09-17 16:34:30 +0700

    dijkstra

---
+++ Dijkstra.Shortet.Path/Dijkstra.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Dijkstra.Shortet.Path
+{
+    public class Dijkstra
+    {
+        private readonly int[,] _graph;
+        public bool IsSingleTarget { get; set; }
+        public int TargetVertex { get; set; }
+        public int Size { get; }
+
+        public Dijkstra(int[,] graph)
+        {
+            _graph = graph;
+            Size = _graph.GetLength(0);
+        }
+
+        public int[] FindShortestPath()
+        {
+            var distanceVertices = Enumerable.Repeat(int.MaxValue, Size).ToArray();
+            var vertices = Enumerable.Range(0, Size).ToArray();
+            distanceVertices[0] = 0;
+            var pickedVertices = new List<int>();
+            var verticesCount = 0;
+
+            do
+            {
+                var nextVertices = vertices.Where(vertext => !pickedVertices.Contains(vertext) && distanceVertices[vertext] != int.MaxValue).ToArray();
+                var pickVertex =
+                    nextVertices.Select(vertex => new { Vertext = vertex, Distance = distanceVertices[vertex] })
+                        .OrderBy(v => v.Distance)
+                        .First()
+                        .Vertext;
+                pickedVertices.Add(pickVertex);
+
+                var adjacentVertices = new List<int>();
+                for (var i = 0; i < Size; i++)
+                {
+                    if (_graph[pickVertex, i] > 0)
+                    {
+                        adjacentVertices.Add(i);
+                    }
+                }
+
+                foreach (var adjacentVertex in adjacentVertices)
+                {
+                    var distanceFromSource = distanceVertices[pickVertex] + _graph[pickVertex, adjacentVertex];
+                    if (distanceFromSource < distanceVertices[adjacentVertex])
+                    {
+                        distanceVertices[adjacentVertex] = distanceFromSource;
+                    }
+                }
+
+                if (IsSingleTarget && pickVertex == TargetVertex)
+                {
+                    return new[] { distanceVertices[TargetVertex] };
+                }
+
+                verticesCount++;
+            } while (verticesCount < Size);
+
+            return distanceVertices;
+        }
+    }
+}
---
+++ Dijkstra.Shortet.Path/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Dijkstra.Shortet.Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dijkstra = new Dijkstra(new[,]
+            {
+                {0, 4, 0, 0, 0, 0, 0, 8, 0},
+                {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                {0, 0, 2, 0, 0, 0, 6, 7, 0}
+            });
+            var result = dijkstra.FindShortestPath();
+            var index = 0;
+            foreach (var distanceVertex in result)
+            {
+                Console.WriteLine(string.Format("Vertex: {0} \tDistance from Source: {1}", index++, distanceVertex));
+            }
+            Console.WriteLine();
+            dijkstra.IsSingleTarget = true;
+            for (var i = 0; i < 9; i++)
+            {
+                dijkstra.TargetVertex = i;
+                Console.WriteLine(string.Format("Vertex: {0} \tDistance from Source: {1}", i, dijkstra.FindShortestPath().First()));
+            }
+            Console.ReadKey();
+        }
+    }
+}
