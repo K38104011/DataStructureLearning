206b6cbe2fa4161c763de80cac1785187a0c7e15
Giang <phamdanghoanggiang@gmail.com>
2017-09-23 14:18:14 +0700

    kruskal

---
+++ Kruskal.Minimum.Spanning.Tree/Kruskal.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kruskal.Minimum.Spanning.Tree
+{
+    public class Kruskal
+    {
+        private readonly int[,] _graph;
+        private readonly ICollection<KeyValuePair<int, Edge>> _sortedEdges;
+        private readonly int[] _vertices;
+        public readonly ICollection<Edge> SpanningTree;
+        public int Size { get; }
+
+        public class Edge
+        {
+            public int Source { get; set; }
+            public int Destination { get; set; }
+        }
+
+        public Kruskal(int[,] graph)
+        {
+            _graph = graph;
+            Size = _graph.GetLength(0);
+            _sortedEdges = InitializeSortedEdge();
+            _vertices = Enumerable.Repeat(-1, Size).ToArray();
+            SpanningTree = new List<Edge>();
+            BuildSpanningTree();
+        }
+
+        private ICollection<KeyValuePair<int, Edge>> InitializeSortedEdge()
+        {
+            var sortedEdges = new List<KeyValuePair<int, Edge>>();
+            for (var i = 0; i < Size; i++)
+            {
+                for (var j = 0; j < Size; j++)
+                {
+                    if (_graph[i, j] > 0)
+                    {
+                        _graph[j, i] = 0;
+                        sortedEdges.Add(new KeyValuePair<int, Edge>(_graph[i, j], new Edge
+                        {
+                            Source = i,
+                            Destination = j
+                        }));
+                    }
+                }
+            }
+            sortedEdges = sortedEdges.OrderBy(item => item.Key).ToList();
+            return sortedEdges;
+        }
+
+        private void BuildSpanningTree()
+        {
+            foreach (var keyValuePair in _sortedEdges)
+            {
+                var sourceVertex = keyValuePair.Value.Source;
+                var destiantionVertex = keyValuePair.Value.Destination;
+                var sourceSet = FindUnionSet(sourceVertex);
+                var destinationSet = FindUnionSet(destiantionVertex);
+                if (sourceSet != destinationSet)
+                {
+                    SpanningTree.Add(keyValuePair.Value);
+                    BuildUnionSet(sourceSet, destinationSet);
+                }
+            }
+        }
+
+        private void BuildUnionSet(int sourceSet, int destinationSet)
+        {
+            _vertices[sourceSet] = destinationSet;
+        }
+
+        private int FindUnionSet(int vertex)
+        {
+            return _vertices[vertex] == -1 ? vertex : FindUnionSet(_vertices[vertex]);
+        }
+
+        public void PrintSortedEdges()
+        {
+            Console.WriteLine("Weight \tSource \tDestination");
+            foreach (var keyValuePair in _sortedEdges)
+            {
+                Console.WriteLine(string.Format("{0} \t{1} \t{2}", keyValuePair.Key, keyValuePair.Value.Source, keyValuePair.Value.Destination));
+            }
+        }
+    }
+}
---
+++ Kruskal.Minimum.Spanning.Tree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Kruskal.Minimum.Spanning.Tree
+{
+    class Program
+    {
+        static void Main()
+        {
+            var kruskal = new Kruskal(new[,]
+            {
+                {0, 4, 0, 0, 0, 0, 0, 8, 0},
+                {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                {0, 0, 2, 0, 0, 0, 6, 7, 0}
+            });
+            kruskal.PrintSortedEdges();
+            foreach (var edge in kruskal.SpanningTree)
+            {
+                Console.WriteLine(string.Format("{0} -- {1}", edge.Source, edge.Destination));
+            }
+            Console.ReadKey();
+        }
+    }
+}
