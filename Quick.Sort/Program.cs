ff9dfcfc87f9770f909641b649f3c5784ba83171
Giang <phamdanghoanggiang@gmail.com>
2017-09-23 17:28:33 +0700

    quick sort

---
+++ Quick.Sort/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Quick.Sort
+{
+    class Program
+    {
+        static void Main()
+        {
+            var qs = new QuickSort(new []{ 10, 7, 8, 9, 1 ,5 });
+            Console.WriteLine(string.Join(",", qs.GetSorted));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Quick.Sort/QuickSort.cs
@@ -0,0 +1,50 @@
+﻿namespace Quick.Sort
+{
+    public class QuickSort
+    {
+        private readonly int[] _orgin;
+
+        public int[] GetSorted
+        {
+            get { return _orgin; }
+        }
+
+        public QuickSort(int[] origin)
+        {
+            _orgin = origin;
+            Sort(0, _orgin.Length - 1);
+        }
+
+        private void Sort(int leftIndex, int rightIndex)
+        {
+            if (leftIndex < rightIndex)
+            {
+                var pivot = Partition(leftIndex, rightIndex);
+                Sort(leftIndex, pivot - 1);
+                Sort(pivot + 1, rightIndex);
+            }
+        }
+
+        private int Partition(int leftIndex, int rightIndex)
+        {
+            var pivot = _orgin[rightIndex];
+            var sortedIndex = leftIndex - 1;
+            for (var i = leftIndex; i < rightIndex; i++)
+            {
+                if (_orgin[i] < pivot)
+                {
+                    Swap(ref _orgin[i], ref _orgin[++sortedIndex]);
+                }
+            }
+            Swap(ref _orgin[sortedIndex + 1], ref _orgin[rightIndex]);
+            return ++sortedIndex;
+        }
+
+        private void Swap(ref int first, ref int second)
+        {
+            first = first + second;
+            second = first - second;
+            first = first - second;
+        }
+    }
+}
